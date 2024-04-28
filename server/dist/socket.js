d29c6002f56bbdcb8fdb376a92607cf7f05d58ee
Suneesh C O <suneeshcotkm@gmail.com>
2024-04-28 20:51:27 +0530

    small bug fixed

---
+++ server/dist/socket.js
@@ -0,0 +1,70 @@
+const { Server } = require('socket.io');
+
+const initializeSocket = (httpServer) => {
+    const io = new Server(httpServer, {
+        cors: {
+            origin: ['http://localhost:5173', 'http://localhost:3000']
+        }
+    });
+
+    let users = [];
+
+    const addUser = (userId, socketId) => {
+        !users.some((user) => user.userId === userId) &&
+            users.push({ userId, socketId });
+    };
+
+    const removeUser = (socketId) => {
+        users = users.filter(user => user.socketId !== socketId);
+    };
+
+    const getUser = (userId) => {
+        return users.find(user => user.userId === userId);
+    };
+
+    io.on("connection", (socket) => {
+        console.log("user connected");
+
+        socket.on("addUser", (userId) => {
+            addUser(userId, socket.id);
+            io.emit("getUsers", users);
+        });
+
+        socket.on("sendMessageStudent", ({ senderId, receiverId, text, mediaUrl }) => {
+            const user = getUser(receiverId);
+
+            if (user) {
+                io.to(user.socketId).emit("getMessageInstructor", {
+                    senderId,
+                    text,
+                    mediaUrl
+                });
+            } else {
+                console.log(`User with userId ${receiverId} not found.`);
+            }
+        });
+
+        socket.on("sendMessageInstructor", ({ senderId, receiverId, text, mediaUrl }) => {
+            const user = getUser(receiverId);
+
+            if (user) {
+                io.to(user.socketId).emit("getMessageStudent", {
+                    senderId,
+                    text,
+                    mediaUrl
+                });
+            } else {
+                console.log(`User with userId ${receiverId} not found.`);
+            }
+        });
+
+        socket.on("disconnect", () => {
+            console.log("a user disconnected");
+            removeUser(socket.id);
+        });
+    });
+};
+
+module.exports = {
+    initializeSocket
+};
