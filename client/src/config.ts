4232ffa03619cafc089e3a17e22a318a9dc95c4d
Suneesh C O <suneeshcotkm@gmail.com>
2024-04-29 15:05:56 +0530

    url changed

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,7 @@
 export const config = {
-    authBaseURL: 'http://localhost:3000/api/auth',
-    adminBaseURL: 'http://localhost:3000/api/admin',
-    studentBaseURL: 'http://localhost:3000/api/student',
-    instructorBaseURL: 'http://localhost:3000/api/instructor',
-    chatBaseUrl:'http://localhost:3000/api/chat'
+    authBaseURL: 'https://eduzones.online/api/auth',
+    adminBaseURL: 'https://eduzones.online/api/admin',
+    studentBaseURL: 'https://eduzones.online/api/student',
+    instructorBaseURL: 'https://eduzones.online/api/instructor',
+    chatBaseUrl:'https://eduzones.online/api/chat'
 };
